07c4655c3fa67763ce639c00a5d33090221b9628
Ferdinand Saufler <mail@saufler.de>
2015-05-31 17:00:54 +0200

    neue Datei:     Makefile 	neue Datei:     README.md 	neue Datei:     main.c

---
+++ 106-raspberrypi-model-info/README.md
@@ -0,0 +1,49 @@
+Building your first Kernel Modul
+================================
+Author: Ferdinand Saufler <mail@saufler.de> <br>
+Date:   30.05.2015
+
+This example describes how to build your first generic kernel driver.
+
+Build the Driver
+----------------
+Just switch to the Driver Directory and type:
+``` bash
+make
+```
+
+Make sure you have the headers for your kernel installed before. On Ubuntu
+you can install them with:
+``` bash
+sudo apt-get install linux-headers-$(uname -r)
+```
+
+On Arch-Linux use Pacman to get the headers with:
+``` bash
+sudo pacman -S linux-headers
+```
+
+The next step is to load the modul in the running kernel, i created a
+makefile-target for you:
+``` bash
+make insmod
+
+# or use
+sudo insmod amazing-driver.ko
+```
+
+After that check dmesg, the Driver should print a message like this:
+``` config
+[ 2138.046530] amazing-driver init_module called
+[ 2138.046532] type: generic
+[ 2138.046532] version: 0.0.1
+[ 2145.073329] amazing-driver mod_exit called.
+```
+
+To unload the Driver simply use the makefile again:
+``` bash
+make rmmod
+
+# or use
+sudo rmmod amazing-driver
+```
---
+++ 106-raspberrypi-model-info/main.c
@@ -0,0 +1,71 @@
+/* Ferdinand Saufler <mail@saufler.de>
+ * 16.03.2015
+ * this program checks /proc/cpuinfo to detect a PI1/PI2  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <file.h>
+
+typedef struct raspberry {
+    char ModelName[254];
+    char Hardware[254];
+    char Revision[5];
+} Raspberry;
+
+int main()
+{
+    //char *filename = "/proc/cpuinfo";
+    char *filename = "/proc/cpuinfo";
+    FILE *f;
+    char line[254];
+
+    /* open the file for reading */
+    f = fopen(filename,"r");
+
+    /* allocate memory for the config struct */
+    Raspberry *info = malloc(sizeof(Raspberry));
+
+    printf("loading values from %s:\n", filename);
+
+    /* iterate over the lines, and scan its contents */
+    while(fgets(line,sizeof(line),f) != NULL)
+    {
+        if(sscanf(line, "model name : %[^\t\n]", info->ModelName) == 1)
+        {
+            printf("Model: %s\n", info->ModelName);
+            continue;
+        }
+        else if(sscanf(line, "Hardware : %s", info->Hardware) == 1)
+        {
+            printf("Hardware: %s\n", info->Hardware);
+            continue;
+        }
+        else if(sscanf(line, "Revision : %s", info->Revision) == 1)
+        {
+            printf("Revision: %s\n", info->Revision);
+            continue;
+        }
+    }
+
+    if(strcmp(info->Hardware,"BCM2708") == 0)
+    {
+        printf("This is a Raspberry Pi 1.\n");
+    }
+    else if(strcmp(info->Hardware,"BCM2836") == 0)
+    {
+        printf("This is a Raspberry Pi 2.\n");
+    }
+    else
+    {
+        printf("Neither a Pi1 or a Pi2.\n");
+    }
+
+    /* close the file f */
+    fclose(f);
+
+    /* cleanup Memory */
+    free(info);
+
+    return 0;
+}
